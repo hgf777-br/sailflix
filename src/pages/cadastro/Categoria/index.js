11190df199c181dd4e59b05d71bab65ae22ac5e7
hgf777-br <hgf777@gmail.com>
2020-07-29 00:47:07 -0300

    teste de envio

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import {Link} from 'react-router-dom';
 
 function CadastroCategoria() {
-    return (
-        <PageDefault>
-            <h1>Cadastrar Categoria</h1>
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
 
-            <Link to="/cadastro/Video">
-                Cadastrar Video
-            </Link>
-        </PageDefault>
-    );
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
