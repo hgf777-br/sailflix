6749ff9310a39bc7d1ceb3c12a277038efe85a21
hgf777-br <hgf777@gmail.com>
2020-07-31 12:19:45 -0300

    mais um....

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React from 'react';
+import propTypes from 'prop-types';
 
-function FormField({ label, type, name, value, onChange }) {
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const fieldID = 'id_$(name)';
   return (
     <div>
-      <label>
+      <label htmlFor={fieldID}>
         {label}
         <input
+          id={fieldID}
           type={type}
           value={value}
           name={name}
@@ -13,7 +18,22 @@ function FormField({ label, type, name, value, onChange }) {
         />
       </label>
     </div>
-  )
+  );
 }
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: propTypes.string.isRequired,
+  type: propTypes.string,
+  name: propTypes.string.isRequired,
+  value: propTypes.string,
+  onChange: propTypes.func,
+
+};
 
 export default FormField;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,46 +2,49 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
-import {ListaCategorias, FormularioCategoria} from './styles.js';
+import { ListaCategorias, FormularioCategoria } from './styles.js';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '',
-  }
+  };
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
-
 
   function setValue(chave, valor) {
     // chave: nome, descricao, bla, bli
     setValues({
       ...values,
       [chave]: valor, // nome: 'valor'
-    })
+    });
   }
 
   function handleChange(infosDoEvento) {
     setValue(
       infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value
+      infosDoEvento.target.value,
     );
   }
 
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
       <FormularioCategoria onSubmit={function handleSubmit(infosDoEvento) {
-          infosDoEvento.preventDefault();
-          if (values.nome && values.descricao) {
-            setCategorias([
-              ...categorias,
-              values
-            ]);
-          }
-          setValues(valoresIniciais)
+        infosDoEvento.preventDefault();
+        if (values.nome && values.descricao) {
+          setCategorias([
+            ...categorias,
+            values,
+          ]);
+        }
+        setValues(valoresIniciais);
       }}
       >
 
@@ -68,28 +71,28 @@ function CadastroCategoria() {
           value={values.cor}
           onChange={handleChange}
         />
-       
-        <button>
+
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </FormularioCategoria>
-      
 
       <ListaCategorias>
-        {categorias.map((categoria, indice) => {
-          return (
-            <li key={`${categoria}${indice}`}>
-              {categoria.nome} | {categoria.descricao}
-            </li>
-          )
-        })}
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria}${indice}`}>
+            {categoria.nome}
+            {' '}
+            |
+            {categoria.descricao}
+          </li>
+        ))}
       </ListaCategorias>
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
