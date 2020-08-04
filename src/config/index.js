1329f682e6d4f2baf9e07543aee506f16ee47bff
hgf777-br <hgf777@gmail.com>
2020-08-03 22:18:15 -0300

    v 1.32

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
-const URL_BACKEND_TOP = 'http://localhost:3080';
+const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
+? 'http://localhost:3080'
+: 'https://sailflix.herokuapp.com';
 
 export default {
-  URL_BACKEND_TOP,
+URL_BACKEND_TOP,
 };
